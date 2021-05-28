4f2581c412ee4bbb5dbc377b4169b856f1228465
filipgia <39337404+filipgia@users.noreply.github.com>
2021-05-28 17:19:06 +0300

    Importing the letters and logging them

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+const bodyParser = require('body-parser')
 const express = require('express')
+
 const app = express()
 const port = 3000
+
+const letters = require('./letters');
+
+console.log(letters);
+
 app.get('/api', (req, res) => {
     res.send('API is working');
 })
