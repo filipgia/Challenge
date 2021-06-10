a9ec900f30aa3259ba3230509da434d8ac5dd696
filipgia <39337404+filipgia@users.noreply.github.com>
2021-06-10 14:18:20 +0300

    removed unnecessery logs and added commends

--- ml.js
+++ ml.js
@@ -2,6 +2,7 @@ const brain = require("brain.js");
 const letters = require("./letters");
 
 module.exports.findLetter = function (hashtags) {
+  //normalizing the data for faster training
   function normalize(string) {
     return string.split("").map(toNumber);
   }
@@ -9,21 +10,21 @@ module.exports.findLetter = function (hashtags) {
   function toNumber(character) {
     return character === "#" ? 1 : 0;
   }
-  console.log(normalize(letters.a));
+  
 
   const net = new brain.NeuralNetwork();
+  //preparing the data for training
   const trainingData = [
     { input: normalize(letters.a), output: { a: 1 } },
     { input: normalize(letters.b), output: { b: 1 } },
     { input: normalize(letters.c), output: { c: 1 } },
     { input: normalize(letters.aWithError), output: { a: 1 } }
   ];
-
+//training the data
   net.train(trainingData, {
-    log: (stats) => console.log(stats),
     errorThresh: 0.0025
   });
-
+//returning the result with highest prob
   const result = brain.likely(normalize(hashtags), net);
 
   return result;
